a9c27525ef4bf74cfc9cdbe2f762c62aa66be92c
osaopmds <osaopmds@PM03600218762.pmdf.local>
2019-09-25 12:14:30 -0300

    Authentication implemented make:auth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -71,8 +71,8 @@
                     @if (Auth::check())
                         <a href="{{ url('/home') }}">Home</a>
                     @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
+                        <a href="{{ url('/login') }}">Sign in</a>
+                        <a href="{{ url('/register') }}">Sign up</a>
                     @endif
                 </div>
             @endif
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,12 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('/home', 'HomeController@index')->name('home');
+});
+
+Auth::routes();
+
+
